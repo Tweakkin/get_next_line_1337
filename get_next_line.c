aa1a01b0a15b439bb3c9ff0c32a6601fee9f00aa
Tweakkin <yahyaboukhmira7@gmail.com>
2025-11-26 11:41:11 +0100

    progress

--- get_next_line.c
+++ get_next_line.c
@@ -6,15 +6,41 @@
 /*   By: yboukhmi <yboukhmi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/25 12:32:05 by yboukhmi          #+#    #+#             */
-/*   Updated: 2025/11/25 12:35:30 by yboukhmi         ###   ########.fr       */
+/*   Updated: 2025/11/25 19:08:30 by yboukhmi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+int	allocate_buff(int fd, char **buff)
+{
+	size_t	fdlen;
+	
+	*buff = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!*buff)
+		return (0);
+	fdlen = read(fd, *buff, BUFFER_SIZE);
+	if (fdlen <= 0)
+	{
+		free(*buff);
+		*buff = NULL;
+		return (0);
+	}
+	(*buff)[fdlen] = '\0';
+	return (fdlen);	
+}
+
 char *get_next_line(int fd)
 {
-	while (1)
-		break;
-	return (NULL);
+	static char	*buff[FD_SIZE];
+	char		*line;
+	
+	if (fd < 0 || fd >= FD_SIZE || BUFFER_SIZE <= 0)
+		return (NULL);
+	if(!buff[fd])
+	{
+		if (allocate_buff(fd, &buff[fd]) == 0)
+			return (NULL);
+	}
+	
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: yboukhmi <yboukhmi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/25 12:32:01 by yboukhmi          #+#    #+#             */
-/*   Updated: 2025/11/25 12:36:37 by yboukhmi         ###   ########.fr       */
+/*   Updated: 2025/11/25 18:53:54 by yboukhmi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,17 @@
 # define GET_NEXT_LINE_H
 
 # include <stdio.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+# define BUFFER_SIZE 1024
+# endif
+
+#ifndef FD_SIZE
+# define FD_SIZE 4096
+#endif
 
 char	*get_next_line(int fd);
 
